9e1e75ef97ab784d55187ee838f4988d843a6289
Stephanie Cheng <sccheng@us.ibm.com>
2015-08-06 13:50:16 -0500

    Removing old docs folder

--- docs/gen/MobileFirst/Java/html/classcom_1_1ibm_1_1mil_1_1summit_1_1database_1_1_cloudant_connector_1_1_user_list.js
+++
@@ -1,5 +0,0 @@
-var classcom_1_1ibm_1_1mil_1_1summit_1_1database_1_1_cloudant_connector_1_1_user_list =
-[
-    [ "name", "classcom_1_1ibm_1_1mil_1_1summit_1_1database_1_1_cloudant_connector_1_1_user_list.html#a7feffd4b829cbf3ab3fbb1acdfbeb7df", null ],
-    [ "products", "classcom_1_1ibm_1_1mil_1_1summit_1_1database_1_1_cloudant_connector_1_1_user_list.html#ab3cf0d593f591eb63cb99d57f66e7465", null ]
-];
--- docs/gen/MobileFirst/Java/html/classcom_1_1ibm_1_1mil_1_1summit_1_1model_1_1_size_to_quantity.js
+++
@@ -1,9 +0,0 @@
-var classcom_1_1ibm_1_1mil_1_1summit_1_1model_1_1_size_to_quantity =
-[
-    [ "getQuantity", "classcom_1_1ibm_1_1mil_1_1summit_1_1model_1_1_size_to_quantity.html#a9ebc5811f2c3c25141351dee75956b05", null ],
-    [ "getSize", "classcom_1_1ibm_1_1mil_1_1summit_1_1model_1_1_size_to_quantity.html#a66f3d0bf41ab9d39225113837b916e72", null ],
-    [ "setQuantity", "classcom_1_1ibm_1_1mil_1_1summit_1_1model_1_1_size_to_quantity.html#aeba317c07c227b613ad34af1bc77afb2", null ],
-    [ "setSize", "classcom_1_1ibm_1_1mil_1_1summit_1_1model_1_1_size_to_quantity.html#a9a24eed3f87162b0ad79ac0910028e15", null ],
-    [ "quantity", "classcom_1_1ibm_1_1mil_1_1summit_1_1model_1_1_size_to_quantity.html#a6c3f195988c371460f46aa41db409d0a", null ],
-    [ "size", "classcom_1_1ibm_1_1mil_1_1summit_1_1model_1_1_size_to_quantity.html#a3cc243bdac326b03a4fd10a75a1fd6fd", null ]
-];
