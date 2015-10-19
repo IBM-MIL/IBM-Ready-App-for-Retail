6f8554e70b07a7fbedbceb4c621e95a080b120fc
Trisha Hanlon <tehanlon@us.ibm.com>
2015-10-19 16:17:08 -0500

    working on pulling out the hard coded values

---
+++ ReadyAppsSummit/server/java/com/ibm/mil/summit/util/Constants.java
@@ -0,0 +1,19 @@
+/*
+ * Licensed Materials - Property of IBM © Copyright IBM Corporation 2015. All
+ * Rights Reserved. This sample program is provided AS IS and may be used,
+ * executed, copied and modified without royalty payment by customer (a) for its
+ * own instruction and study, (b) in order to develop applications designed to
+ * run with an IBM product, either for customer's own internal use or for
+ * redistribution by customer, as part of such an application, in customer's own
+ * products.
+ */
+
+package com.ibm.mil.ready.app.perch.utils;
+
+public class Constants {
+	/******* Constants for the CloudantService class ******/
+	public final static String ACCOUNT_KEY = "CLOUDANT_ACCOUNT";
+	public final static String USERNAME_KEY = "CLOUDANT_USERNAME";
+	public final static String PASSWORD_KEY = "CLOUDANT_PASSWORD";
+	public final static String DB_KEY = "CLOUDANT_DB_NAME";
+}
